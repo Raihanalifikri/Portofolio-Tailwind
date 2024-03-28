63d852a21d6022e4ba29dea5df710fd3b5ec2f24
Raihan Ali <m.raihanalifikri@gmail.com>
2024-03-28 13:58:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",  "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Poppins' : ['Poppins'],
+      'Shadows Into Light' : ['Shadows Into Light'],
+      'Comic Neue' : ['Comic Neue'],
+      'Anta' : ['Anta']
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
